b990d357b029351cf1eeee795f3678d25cea3654
Andi Staub <andi.staub@gmail.com>
2016-04-12 09:07:53 +0200

    Take blog helper out of helpers.

--- config.rb
+++ config.rb
@@ -31,20 +31,22 @@ activate :blog do |blog|
   end
 end
 
-# Helpers are in helpers/custom_helpers
+# Helpers
 SUMMARY_START = "SUMMARY_START"
 SUMMARY_END = "SUMMARY_END"
 
-def navigation_list(navigation, li_class: 'list-inline-item', a_class: '')
-  navigation.map do |label, path|
-    content_tag :li, class: li_class do
-      link_to label, "/#{path}", class: a_class
-    end
-  end.join
-end
+helpers do
+  def navigation_list(navigation, li_class: 'list-inline-item', a_class: '')
+    navigation.map do |label, path|
+      content_tag :li, class: li_class do
+        link_to label, "/#{path}", class: a_class
+      end
+    end.join
+  end
 
-def cleanup_summary_start_end_separator(html)
-  html.sub(SUMMARY_START, "").sub(SUMMARY_END, "")
+  def cleanup_summary_start_end_separator(html)
+    html.sub(SUMMARY_START, "").sub(SUMMARY_END, "")
+  end
 end
 
 # Build-specific configuration
