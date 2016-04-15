955a073bb42448079a8c4742d18efcb8d9599fae
Andi Staub <andi.staub@gmail.com>
2016-04-15 09:56:43 +0200

    Use middleman favicon generator for favicons.

--- config.rb
+++ config.rb
@@ -64,6 +64,22 @@ end
 
 # Build-specific configuration
 configure :build do
+  activate :favicon_maker do |f|
+    f.template_dir  = 'source/images'
+    f.icons = {
+      "favicon.svg" => [
+        { icon: "apple-touch-icon-precomposed.png", size: "152x152" },
+        { icon: "favicon.png", size: "152x152" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+        { icon: "mstile-70x70.png", size: "70x70" },
+        { icon: "mstile-144x144.png", size: "144x144" },
+        { icon: "mstile-150x150.png", size: "150x150" },
+        { icon: "mstile-310x310.png", size: "310x310" },
+        { icon: "mstile-310x150.png", size: "310x150" }
+      ]
+    }
+  end
+
   # Minify CSS on build
   # activate :minify_css
 
