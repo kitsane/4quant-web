be2648e953f27eb5660faf858937339720cd055f
Casaper <vollenweider@gmail.com>
2016-07-06 16:06:19 +0200

    feat(view): testemonial carousel

---
+++ source/javascripts/main.js
@@ -0,0 +1,9 @@
+var testemonialCarouselOpts = {
+  interval: 2000,
+  pause: "hover",
+  wrap: true,
+  keyboard: true,
+}
+
+$('#testemonial-carousel-one').carousel(testemonialCarouselOpts);
+$('#testemonial-carousel-two').carousel(testemonialCarouselOpts);
