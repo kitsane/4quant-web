65bef1bce07de21fbe4a8d0c293f08e1697a693a
Casaper <vollenweider@gmail.com>
2016-07-26 16:28:32 +0200

    feat(navbar): navbar changed
    - it is now initialized by js
    - can be closed by clicking into the empty space
    - navbar height is beeing supplied by variable

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,12 +1,22 @@
 $(function($) {
+    $('#mainNavigation .navbar').click(function(event) {
+        $('#mainNavigation').modal('toggle');
+    });
+    $('#menu-toggler').click(function(event){
+        $('#mainNavigation').modal({
+            keyboard: true
+        });
+    });
+    $('#menu-toggler-close').click(function(event) {
+        $('#mainNavigation').modal('toggle');
+    });
     const testemonialCarouselOpts = {
-  interval: 2000,
-  pause: "hover",
-  wrap: false,
-  keyboard: false,
-}
+        interval: 2000,
+        pause: "hover",
+        wrap: false,
+        keyboard: false,
+    }
 
-$('#testemonial-carousel-one').carousel(testemonialCarouselOpts);
-$('#testemonial-carousel-two').carousel(testemonialCarouselOpts);
-$('textarea').autogrow();
+    $('#testemonial-carousel-one').carousel(testemonialCarouselOpts);
+    $('#testemonial-carousel-two').carousel(testemonialCarouselOpts);
 });
